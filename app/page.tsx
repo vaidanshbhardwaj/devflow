791e6a459e202d5b013b7e4e73a341c8ce026fd0
vaidanshbhardwaj <vaidanshbhardwaj@gmail.com>
2024-03-14 05:27:52 +0530

    Added auth

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS</h1>
-      <h2 className="h2-bold">NextJS</h2>
-      <h3 className="h3-bold">NextJS</h3>
-    </div>
-  );
-}
