1475500e4011c4dd7096236b9375389824728124
vaidanshbhardwaj <vaidanshbhardwaj@gmail.com>
2024-03-21 01:41:40 +0530

    Code before active lesson

---
+++ components/shared/search/LocalSearchbar.tsx
@@ -0,0 +1,17 @@
+interface CustomInputProps {
+  placeholder: string;
+  route: string;
+  iconPosition: string;
+  imgSrc: string;
+  otherClasses: string;
+}
+const LocalSearchbar = ({
+  placeholder,
+  route,
+  iconPosition,
+  imgSrc,
+  otherClasses,
+}: CustomInputProps) => {
+  return <div>LocalSearchbar</div>;
+};
+export default LocalSearchbar;
