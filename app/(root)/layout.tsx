ce637bd9464750c5c15ed6f24867e7009a55da80
vaidanshbhardwaj <vaidanshbhardwaj@gmail.com>
2024-03-21 01:05:41 +0530

    added first section

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import RightSidebar from "@/components/shared/RightSidebar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import LeftSidebar from "@/components/shared/search/LeftSidebar";
 import React from "react";
@@ -7,11 +8,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className="background-light850_dark100 relative">
       <Navbar />
       <div className="flex">
-      <LeftSidebar />
+        <LeftSidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14 ">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
-        RightSIdebar
+        <RightSidebar />
       </div>
       Toaster
     </main>
