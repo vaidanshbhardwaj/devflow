6cfaa60a0513ef03780be6438e59655879071fa6
vaidanshbhardwaj <vaidanshbhardwaj@gmail.com>
2024-03-22 00:01:38 +0530

    file changes

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface QuestionProps {
   _id: string;
   title: string;
@@ -25,6 +27,20 @@ const QuestionCard = ({
   answers,
   createdAt,
 }: QuestionProps) => {
-  return <div>QuestionCard</div>;
+  return (
+    <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
+      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+        <div>
+          <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
+            {String(createdAt)}
+          </span>
+          <Link href={`/question/${_id}`}>
+            <h3>{title}</h3>
+            {/* //!! 24:45 */}
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default QuestionCard;
