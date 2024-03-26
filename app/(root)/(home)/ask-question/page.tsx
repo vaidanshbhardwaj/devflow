991482effd4f4d03487b02e52b4e482c5701b043
vaidanshbhardwaj <vaidanshbhardwaj@gmail.com>
2024-03-26 07:42:04 +0530

    empty

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -1,4 +1,13 @@
-const page = () => {
-  return <div>Ask Question</div>;
+import Question from "@/components/forms/Question";
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
-export default page;
+export default Page;
